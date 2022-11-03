7a438be6e4f3d4339475624a9b24ff5adc869901
luccasecco <luccasecco@hotmail.com>
2022-11-03 20:31:44 -0300

    config: adiciona e estiliza statusbar

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
-import { Text, View } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
 
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
-    <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
+    <View style={{alignItems: 'center', justifyContent: 'center', flex: 1, backgroundColor: '#202024'}}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Text>Ignite Gym!</Text> : <View></View>}
     </View>
   );
