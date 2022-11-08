7ce6a46c36c1930f457cbae928a40d0f484e2acb
luccasecco <luccasecco@hotmail.com>
2022-11-08 20:08:01 -0300

    Add lib para lidar com svg no react-native

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,10 +1,16 @@
 import BackgroundImg from '@assets/background.png'
-import { Image, VStack } from 'native-base'
+import LogoSvg from '@assets/logo.svg'
+import { Center, Image, Text, VStack } from 'native-base'
 
 export function SignIn(){
   return(
     <VStack flex={1} bg="gray.700">
       <Image src={BackgroundImg} alt="Pessoas treinando" resizeMode="contain" position="absolute"/>
+      
+      <Center my={24}>
+        <LogoSvg />
+        <Text color="gray.100" fontSize="sm">Treine sua mente e seu corpo</Text>
+      </Center>
     </VStack>
   )
 }
